b70f8200e851fe84d414f75dfa8473eb9a51fde2
maksymPr424 <154377610+maksymPr424@users.noreply.github.com>
2024-07-11 10:16:11 +0200

    Style for React swiper

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,42 +2,52 @@ import MovieItem from "../MovieItem/MovieItem";
 import { useLocation } from "react-router-dom";
 
 import "swiper/css";
+import "swiper/css/scrollbar";
 import "swiper/css/pagination";
 import "swiper/css/keyboard";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Mousewheel, Keyboard } from "swiper/modules";
+import {
+  Mousewheel,
+  Keyboard,
+  Scrollbar,
+  Pagination,
+  Navigation,
+} from "swiper/modules";
 import css from "./MovieList.module.css";
 import clsx from "clsx";
 
 export default function MovieList({ movies }) {
   const location = useLocation();
   return (
-    <Swiper
-      slidesPerView={4}
-      centeredSlides={true}
-      spaceBetween={30}
-      grabCursor={true}
-      loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      keyboard={{ enabled: true }}
-      mousewheel={true}
-      modules={[Pagination, Mousewheel, Keyboard]}
-      className={clsx(css.swiper, "mySwiper")}
-    >
-      {movies.map(({ id, title, poster_path }) => {
-        return (
-          <SwiperSlide className={css.item} key={id}>
-            <MovieItem
-              id={id}
-              title={title}
-              location={location}
-              poster={poster_path}
-            />
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <div className={css.container}>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        loop={true}
+        keyboard={{ enabled: true }}
+        mousewheel={true}
+        scrollbar={{
+          hide: true,
+        }}
+        pagination={{
+          type: "fraction",
+        }}
+        modules={[Pagination, Navigation, Mousewheel, Keyboard, Scrollbar]}
+        className={clsx(css.swiper, "mySwiper")}
+      >
+        {movies.map(({ id, title, poster_path }) => {
+          return (
+            <SwiperSlide className={css.item} key={id}>
+              <MovieItem
+                id={id}
+                title={title}
+                location={location}
+                poster={poster_path}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 }
